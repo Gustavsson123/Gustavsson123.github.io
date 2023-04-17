a79208512a846049a111af8cc57b19a33aa41650
Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
2023-04-17 14:10:04 +0200

    test

---
+++ portfolio/src/resumeData.js
@@ -0,0 +1,125 @@
+let resumeData = {
+  "imagebaseurl": "https://Gustavsson123.github.io/portfolio",
+  "name": "Vilhelm Gustavsson",
+  "role": "Software development student",
+  "linkedinId": "Your LinkedIn Id",
+  "skypeid": "Your skypeid",
+  "roleDescription": " I like dabbling in various parts of frontend development and like to learn about new technologies, just relax or simply play games in my free time.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "http://github.com/rbhatia46",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "skype",
+      "url": "http://twitter.com/rbhatia46",
+      "className": "fa fa-twitter"
+    }
+  ],
+  "aboutme": "I am a software development student from Sweden. During my passed three years in school I have primarly worked on project using programming languages such as python, html, css and javascript. I am currently studying a fourth year of high school at NTI where I get to work on projects in groups using Scrum.",
+  "address": "Sweden",
+  "website": "https://Gustavsson123.github.io/portfolio",
+  "education": [
+    {
+      "UniversityName": "The LNM Insitute of Information Technology",
+      "specialization": "Some specialization",
+      "MonthOfPassing": "Aug",
+      "YearOfPassing": "2020",
+      "Achievements": "Some Achievements"
+    },
+    {
+      "UniversityName": "Some University",
+      "specialization": "Some specialization",
+      "MonthOfPassing": "Jan",
+      "YearOfPassing": "2018",
+      "Achievements": "Some Achievements"
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "Some Company",
+      "specialization": "Some specialization",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2018",
+      "Achievements": "Some Achievements"
+    },
+    {
+      "CompanyName": "Some Company",
+      "specialization": "Some specialization",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2018",
+      "Achievements": "Some Achievements"
+    }
+  ],
+  "skillsDescription": "My Skills",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "JavaScript"
+    },
+    {
+      "skillname": "Nodejs"
+    },
+    {
+      "skillname": "MongoDB"
+    },
+    {
+      "skillname": "Python"
+    },
+    {
+      "skillname": "Selenium"
+    },
+    {
+      "skillname": "MySQL"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "Florist Website",
+      "description": "This project was made for a made up Florist company. It was made with HTML, CSS and JavaScript.",
+      "imgurl": "images/portfolio/project1.png",
+      "link": "https://ntig-uppsala.github.io/florist-blaklinten/"
+    },
+    {
+      "name": "Pizzeria Raffiki",
+      "description": " This project was made for a made up Pizzeria. It was made with HTML, CSS and JavaScript.",
+      "imgurl": "images/portfolio/project2.png"
+    },
+    {
+      "name": "Admin Närvaro",
+      "description": "This project is made for NTI Uppsala. It was made with Next.JS, TaildwindCSS, express and MongoDB.",
+      "imgurl": "images/portfolio/project3.png"
+    },
+    {
+      "name": "POS project",
+      "description": " This project is made based on real POS systems. It was made with C#, .NET and the framework WinForms.",
+      "imgurl": "images/portfolio/project4.png"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
